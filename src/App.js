221f6a2d7d9320bcb224e25be87f3376fda1d4d7
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2017-03-07 00:04:52 -0800

    new update to passing down a static name

--- src/App.js
+++ src/App.js
@@ -1,16 +1,45 @@
 import React, { Component } from 'react';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import CompanyName from './components/CompanyName';
 import './App.css';
 
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
+
+import CompanyName from './components/CompanyName';
+import Companies from './data/Companies';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+injectTapEventPlugin();
+
+const style = {
+  margin: 12,
+};
+
+function randomNumber() {
+  Math.floor(Math.random() * 10 + 1);
+}
+
+const id = randomNumber();
+const company = "Foodee";
+
 class App extends Component {
+  constructor() {
+    super();
+    this.handleClick = this.handleClick.bind(this);
+    this.state = {company};
+  }
+  handleClick() {
+    randomNumber();
+  }
   render() {
     return (
       <MuiThemeProvider>
         <div className="app">
           <div className="contents">
             <div className="business-name">
-              <CompanyName />
+              <CompanyName name={this.state.company}/>
+            </div>
+            <div className="main-button">
+              <RaisedButton label="New Name" secondary={true} style={style} onClick={this.handleClick()} />
             </div>
           </div>
         </div>
--- src/components/CompanyName.js
+++ src/components/CompanyName.js
@@ -1,34 +1,11 @@
 import React, { Component } from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
-import Companies from '../data/Companies';
 
-const style = {
-  margin: 12,
-};
-
-  const id = Math.floor(Math.random() * 10) + 1;
-  const company = Companies.filter((company) => company.id === id)[0];
-
-class CompanyName extends Component {
-  constructor() {
-    super();
-    this.state = {company};
-  }
-  handleClick() {
-    const id = Math.floor(Math.random() * 10) + 1;
-    const company = Companies.filter((company) => company.id === id)[0];
-    this.setState({company});
-  }
-
+export default class CompanyName extends Component {
   render() {
     return (
       <div>
-        <h2>{this.state.company.name}</h2>
-
-        <RaisedButton label="New Name" secondary={true} style={style} onClick={() => this.handleClick()} />
+        <h2>{this.props.name}</h2>
       </div>
     );
   }
 }
-
-export default CompanyName;
