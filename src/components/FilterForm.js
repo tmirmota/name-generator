521d30f2a52d06f064c679405b61f9bff22fb172
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2017-03-24 01:17:36 -0700

    Added Formsy form and pushed data to state

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -8,12 +8,37 @@ import { FormsyText } from 'formsy-material-ui/lib';
 import RaisedButton from 'material-ui/RaisedButton';
 
 class FilterForm extends Component {
+  state = {
+    canSubmit: false,
+    data: []
+  }
+
+  enableButton = () => {
+    this.setState({
+      canSubmit: true
+    });
+  }
+  disableButton = () => {
+    this.setState({
+      canSubmit: false
+    });
+  }
+
+  submitForm = (data) => {
+    this.setState({data});
+  }
+
   render() {
     return (
-      <Formsy.Form>
-        <FormsyText name="Filter" ref={(ref) => {this.inputTextRef = ref;} } floatingLabelText="Filter" />
-
-        <RaisedButton type="submit" label="submit" onClick={()=> { alert(this.inputTextRef.state._value)}} />
+      <Formsy.Form
+        onValid={this.enableButton}
+        onInvalid={this.disableButton}
+        onValidSubmit={this.submitForm}>
+          <FormsyText
+            name="filterName"
+            required
+            floatingLabelText="Filter Name" />
+          <RaisedButton type="submit" label="submit" disabled={!this.state.canSubmit} />
       </Formsy.Form>
     )
   }
