0b32d0bc93cca4b8e292595aaf9f203e8d543811
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2017-03-23 20:38:58 -0700

    Meetup with Eric and Nelson

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Field, reduxForm } from 'redux-form';
 
-import FilterList from './FilterList';
+import FilterForm from './FilterForm';
 
 // material-ui Components
 import TextField from 'material-ui/TextField';
@@ -31,37 +31,7 @@ class Filter extends Component {
   render() {
     return (
       <div className="grid-2">
-
-        <form>
-          <div className="row1 column1-2">
-            <Field name="filterName" component={renderFilterName} label="Filter Name" />
-          </div>
-
-          <div className="row2 column1-2">
-            <DropDownMenu value={this.state.include} onChange={this.includeChange}>
-              <MenuItem value={true} primaryText="Include" />
-              <MenuItem value={false} primaryText="Exclude" />
-            </DropDownMenu>
-            <DropDownMenu value={this.state.restrict} onChange={this.restrictChange}>
-              <MenuItem value={1} primaryText="Equal to (=)" />
-              <MenuItem value={2} primaryText="Contains" />
-              <MenuItem value={3} primaryText="Starts with" />
-            </DropDownMenu>
-          </div>
-
-          <div className="row3 column1-2">
-            <TextField hintText="Example: 'Br'"  />
-          </div>
-
-          <div className="row4 column1-2">
-            <RaisedButton label="Save" type="submit" handleClick={() => this.props.handleClick()}/>
-            <FlatButton label="Cancel" onClick={() => this.props.onClick()} />
-          </div>
-        </form>
-
-        <div className="row5 column1-2">
-          <FilterList />
-        </div>
+          <FilterForm />
       </div>
     );
   }
--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -1,1 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+// Formsy
+import Formsy from 'formsy-react';
+import { FormsyText } from 'formsy-material-ui/lib';
+
+// material-ui
+import RaisedButton from 'material-ui/RaisedButton';
+
+class FilterForm extends Component {
+  render() {
+    return (
+      <Formsy.Form>
+        <FormsyText name="Filter" ref={(ref) => {this.inputTextRef = ref;} } floatingLabelText="Filter" />
+
+        <RaisedButton type="submit" label="submit" onClick={()=> { alert(this.inputTextRef.state._value)}} />
+      </Formsy.Form>
+    )
+  }
+}
+
+export default FilterForm;
