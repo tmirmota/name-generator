09d43e6fda9897932c3f1c0421fa326b61e5a01e
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2017-03-24 23:54:29 -0700

    added all form fields for dropdown

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -31,23 +31,36 @@ class FilterForm extends Component {
 
   render() {
     return (
-      <Formsy.Form
-        onValid={this.enableButton}
-        onInvalid={this.disableButton}
-        onValidSubmit={this.submitForm}>
-          <FormsyText
-            name="filterName"
-            required
-            floatingLabelText="Filter Name" />
-          <FormsySelect
-            name="included"
-            required
-            value={this.true}>
-              <MenuItem value={true} primaryText="Included" />
-              <MenuItem value={false} primaryText="Excluded" />
-           </FormsySelect>
-          <RaisedButton type="submit" label="submit" disabled={!this.state.canSubmit} />
-      </Formsy.Form>
+      <div className="text-left">
+        <Formsy.Form
+          onValid={this.enableButton}
+          onInvalid={this.disableButton}
+          onValidSubmit={this.submitForm}>
+            <FormsyText
+              name="filterName"
+              required
+              floatingLabelText="Filter Name" />
+            <FormsySelect
+              name="included"
+              required
+              value={true} >
+                <MenuItem value={true} primaryText="Included" />
+                <MenuItem value={false} primaryText="Excluded" />
+             </FormsySelect>
+             <FormsySelect
+               name="condition"
+               required
+               hintText="Select a condition" >
+                 <MenuItem value={'contains'} primaryText="Contains" />
+                 <MenuItem value={'start'} primaryText="Starts with" />
+              </FormsySelect>
+              <FormsyText
+                name="filterValue"
+                required
+                hintText="Example: value" />
+            <RaisedButton type="submit" label="submit" disabled={!this.state.canSubmit} />
+        </Formsy.Form>
+      </div>
     )
   }
 }
