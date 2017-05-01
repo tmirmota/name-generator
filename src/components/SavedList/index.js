467fe34ee148838fdf50e0861bda551a605214b9
Thomas Mirmotahari <thomas.mirmotahari@gmail.com>
2017-05-01 01:54:14 -0700

    added red button and capitalization

--- src/components/SavedList/index.js
+++ src/components/SavedList/index.js
@@ -1,22 +1,34 @@
 import React from 'react';
+import _ from 'lodash';
 
 // Material UI Components
 import { List, ListItem } from 'material-ui/List';
 import DeleteForever from 'material-ui/svg-icons/action/delete-forever';
 
+// Inline Styling
+const style = {
+  color: 'white'
+}
+
 export const SavedList = ({ savedCompanies, removeCompany }) => {
   return (
     <section className="container">
-      <List>
-        {savedCompanies.map(company => {
-          return (
-            <ListItem
-              key={company}
-              primaryText={company}
-              rightIcon={<DeleteForever onClick={() => removeCompany(company)} />} />
-            );
-        })}
-      </List>
+      <div className="row">
+        <div className="col-md-6 mx-auto">
+          <List>
+            {savedCompanies.map(company => {
+              const name = _.capitalize(company)
+              return (
+                <ListItem
+                  key={company}
+                  primaryText={name}
+                  style={style}
+                  rightIcon={<DeleteForever onClick={() => removeCompany(company)} hoverColor='red' />} />
+                );
+            })}
+          </List>
+        </div>
+      </div>
     </section>
   );
 }
